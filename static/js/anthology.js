a66a56eb19ba19ca5b5867bad7aadceedd41d0e3
Nensi Gjata <Nensi@Nensis-MacBook-Pro.local>
2020-08-28 12:26:46 -0400

    update server

--- static/js/anthology.js
+++ static/js/anthology.js
@@ -1,4 +1,8 @@
 $(function () {
     document.querySelector(".nav-link[href='/anthology.html']").classList.add("active");
     document.querySelector("footer").style.backgroundColor = "transparent";
+    document.querySelector("footer").style.color = "#FFE7A2";
+    document.querySelectorAll("footer a").forEach(function(el) {
+        el.style.color = "#FFE7A2"; 
+        })
 });
